0fe4c0cf2de5dccf2e2bbc8e22865616381dcf44
ipatate <faramazpat@gmail.com>
2023-07-08 09:47:32 +0200

    add i18n, fix styles

--- patterns/partners.php
+++ patterns/partners.php
@@ -7,8 +7,8 @@
  */
 ?>
 
-<!-- wp:group {"backgroundColor":"quinary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-quinary-background-color has-background">
+<!-- wp:group {"backgroundColor":"quinary","layout":{"type":"constrained"}, "style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-group has-quinary-background-color has-background" style="margin-top:var(--wp--preset--spacing--30)">
   <!-- wp:heading {"textAlign":"center","textColor":"light-blue"} -->
   <h2 class="wp-block-heading has-text-align-center has-light-blue-color has-text-color">Les partenaires du club</h2>
   <!-- /wp:heading -->
